d7f3496f6d95806627e06539450772571d49cdb9
robertarfa <roberta_rfa@hotmail.com>
2020-04-01 17:30:03 -0300

    aprendendo querySelector e textContent

---
+++ 30032020/js/calculo-idade.js
@@ -0,0 +1,9 @@
+function pulaLinha(){
+   document.write("<br>")
+};
+
+var extrairIdade = document.querySelector('#idade');
+var idade = extrairIdade.textContent;
+
+console.log(extrairIdade)
+console.log(idade)
---
+++ 30032020/js/titulo.js
@@ -0,0 +1,6 @@
+
+var dia = "31/03/2020";
+var aula = 2;
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent=("Aula " + aula + " - " + dia);
