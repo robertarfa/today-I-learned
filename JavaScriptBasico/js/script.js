ed9ee1f4b8da5e2cadd83145011dd4bc8b6aaa33
robertarfa <roberta_rfa@hotmail.com>
2020-05-15 13:27:01 -0300

    JavaScriptBasico

---
+++ JavaScriptBasico/js/script.js
@@ -0,0 +1,12 @@
+console.log('Olá, mundo');
+
+var title = document.querySelector('h1');
+title.textContent = 'O título foi modificado';
+
+console.log(title);
+
+console.log(typeof 4);
+console.log(typeof 'Roberta');
+console.log(typeof true);
+console.log(typeof { id: 1, name: 'Roberta' });
+console.log(typeof [1, 2, 3]);
