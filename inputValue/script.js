de4279e31627267a0ac2f65488a6ad73d40241d9
robertarfa <roberta_rfa@hotmail.com>
2020-05-15 18:57:53 -0300

    Calculo com input value

--- inputValue/script.js
+++ inputValue/script.js
@@ -1,8 +1,11 @@
 var a = document.querySelector('.input1');
 var b = document.querySelector('.input2');
 var showInput = document.querySelector('.showInput');
+var buttonElement = document.querySelector('.button');
 
 function calculo() {
   var sum = parseInt(a.value) + parseInt(b.value);
   showInput.innerHTML = `A soma dos valores é ${sum}`;
 }
+
+buttonElement.onclick = calculo;
