237554f4dceac1777ffc250cca58c4535e0f815f
robertarfa <roberta_rfa@hotmail.com>
2020-05-17 21:11:37 -0300

    forms first class

---
+++ form/script.js
@@ -0,0 +1,5 @@
+console.log('17/05/2020')
+
+var input1 = document.querySelector('#input1')
+input1.value =
+  'Quando é input se utiliza value e não textcontent, porque textcontent é para pegar valores entre tags'
