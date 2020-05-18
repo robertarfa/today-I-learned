f4802a30cc72f34ee5b4f295e6627ee5015a6594
robertarfa <roberta_rfa@hotmail.com>
2020-05-17 21:49:38 -0300

    form length counter and prevent default

--- form/script.js
+++ form/script.js
@@ -1,5 +1,29 @@
 console.log('17/05/2020')
 
-var input1 = document.querySelector('#input1')
-input1.value =
-  'Quando é input se utiliza value e não textcontent, porque textcontent é para pegar valores entre tags'
+//Garantir que o JS só vai manipular o HTML quando os elementos estiverem carregados na página
+window.addEventListener('load', start)
+
+function start() {
+  console.log('Página totalmente carregada')
+
+  var nameInput = document.querySelector('#nameInput')
+  nameInput.addEventListener('keyup', countName)
+
+  var form = document.querySelector('form')
+  form.addEventListener('submit', preventSubmit)
+}
+
+function countName(event) {
+  console.log(event)
+  var count = event.target.value
+
+  var nameLength = document.querySelector('#nameLength')
+  nameLength.textContent = count.length
+}
+
+function preventSubmit(event) {
+  event.preventDefault()
+
+  var nameInput = document.querySelector('#nameInput')
+  alert('Nome ' + nameInput.value + ' cadastrado com sucesso')
+}
