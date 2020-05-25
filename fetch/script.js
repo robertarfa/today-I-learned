b7d94ab7282fa9735daf97869fb6bc20fafca326
robertarfa <roberta_rfa@hotmail.com>
2020-05-25 09:46:31 -0300

    Async/Await ShowData

--- fetch/script.js
+++ fetch/script.js
@@ -3,27 +3,17 @@
 window.addEventListener('load', function () {
   //   doFetch()
   doFetchAsync()
+
   //   executeDivisionPromiseAsyncAwait()
 })
-
-//Promise
-function doFetch() {
-  fetch('https://api.github.com/users/robertarfa')
-    .then((resource) => {
-      resource.json().then((data) => {
-        showData(data)
-      })
-    })
-    .catch((error) => {
-      console.log('Erro na requisição')
-    })
-}
 
 //Async/Await
 async function doFetchAsync() {
   const github = await fetch('https://api.github.com/users/robertarfa')
-  const json = github.json()
-  console.log(json)
+  const data = github.json().then((data) => {
+    console.log(data)
+    showData(data)
+  })
 }
 
 function showData(data) {
@@ -31,20 +21,4 @@ function showData(data) {
   login.textContent = `Login: ${data.login}`
   var name = document.querySelector('#name')
   name.textContent = data.name
-}
-
-//Promise
-function divisionPromise(a, b) {
-  return new Promise((resolve, reject) => {
-    if (b === 0) {
-      reject('Não é possível dividir por 0')
-    }
-    resolve(a / b)
-  })
-}
-
-//Async/Await
-async function executeDivisionPromiseAsyncAwait() {
-  const division = await divisionPromise(12, 2)
-  console.log(division)
 }
