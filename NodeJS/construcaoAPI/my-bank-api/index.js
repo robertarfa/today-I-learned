c26ffe8678f31699acf95cecc9e81222783c415e
robertarfa <roberta_rfa@hotmail.com>
2020-06-16 13:10:08 -0300

    update

--- NodeJS/construcaoAPI/my-bank-api/index.js
+++ NodeJS/construcaoAPI/my-bank-api/index.js
@@ -2,20 +2,35 @@ const express = require('express')
 const fs = require('fs').promises
 const app = express()
 const accountsRouter = require("./routes/accounts.js")
+const winston = require('winston')
 
 global.fileName = 'accounts.json'
+
+const { combine, timestamp, label, printf } = winston.format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+	return `${timestamp} [${label}] ${level}: ${message}`
+})
+
+global.logger = winston.createLogger({
+	level: "silly",
+	transports: [
+		new (winston.transports.Console)(),
+		new (winston.transports.File)({ filename: 'my-bank-api.log' })
+	],
+	format: combine
+})
 
 //tem que informar para o express que está utilizando o json
 app.use(express.json())
 app.use('/account', accountsRouter)
+
+
 //writeFile sempre vai apagar o conteúdo do arquivo e colocar um conteúdo novo
 //appendFile não vai apagar, vai adicionar o novo conteúdo
-
-
 app.listen(3000, async () => {
 	try {
 		await fs.readFile(global.fileName, 'utf8')
-		console.log('Api running')
+		logger.info('Api running')
 
 	} catch (err) {
 
@@ -24,7 +39,7 @@ app.listen(3000, async () => {
 			accounts: []
 		}
 		fs.writeFile(global.fileName, JSON.stringify(createJsonFile)).catch(err => {
-			console.log('File was not created')
+			logger.err(err)
 		})
 	}
 })
